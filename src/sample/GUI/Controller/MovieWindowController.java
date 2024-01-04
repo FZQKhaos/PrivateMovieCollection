a0da3245207bdff6f95e2b10cf3560df72da0aae
FZQKhaos <casjen01@easv365.dk>
2024-01-04 10:49:40 +0100

    Created MovieWindow + Controller

---
+++ src/sample/GUI/Controller/MovieWindowController.java
@@ -0,0 +1,22 @@
+package sample.GUI.Controller;
+
+import javafx.event.ActionEvent;
+
+public class MovieWindowController {
+
+    public void onActionNewMovie(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionRemoveMovie(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionAddRemoveCategory(ActionEvent actionEvent) {
+
+    }
+
+    public void onActionAddEditUR(ActionEvent actionEvent) {
+        
+    }
+}
--- src/sample/GUI/Main.java
+++ src/sample/GUI/Main.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import sample.BE.Category;
-import sample.BE.Movie;
 
 public class Main extends Application {
     public static void main(String[] args) {
@@ -15,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MasterWindow.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MovieWindow.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
