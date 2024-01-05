ec7f0d213f7a424ca3675d1e1f5b20c60de58f6d
FZQKhaos <casjen01@easv365.dk>
2024-01-05 10:04:06 +0100

    Open NewMovieWindow + UserRating

--- src/sample/GUI/Controller/MovieWindowController.java
+++ src/sample/GUI/Controller/MovieWindowController.java
@@ -8,11 +8,19 @@ import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 
+import java.io.IOException;
+
 
 public class MovieWindowController {
 
-    public void onActionNewMovie(ActionEvent actionEvent) {
+    public void onActionNewMovie(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/NewMovieWindow.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
 
+        stage.setTitle("New Movie");
+        stage.show();
     }
 
     public void onActionRemoveMovie(ActionEvent actionEvent) {
@@ -23,7 +31,13 @@ public class MovieWindowController {
 
     }
 
-    public void onActionAddEditUR(ActionEvent actionEvent) {
-        
+    public void onActionAddEditUR(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/UserRating.fxml"));
+        Parent root = loader.load();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+
+        stage.setTitle("Add/Edit User Rating");
+        stage.show();
     }
 }
