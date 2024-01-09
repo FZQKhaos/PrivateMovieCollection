a8504dbcd5de076d7e41c00290c09c4eb79e285a
FZQKhaos <casjen01@easv365.dk>
2024-01-09 11:57:17 +0100

    AddCategoryToMovieWindow + Controller

---
+++ src/sample/GUI/Controller/AddCategoryToMovieController.java
@@ -0,0 +1,23 @@
+package sample.GUI.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class AddCategoryToMovieController {
+
+    @FXML
+    private ComboBox cbMovies;
+
+    @FXML
+    private ListView lstCategory;
+
+    @FXML
+    private Label lblStatus;
+
+    public void onActionAdd(ActionEvent actionEvent) {
+        
+    }
+}
