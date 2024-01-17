532dc7f3a0b9ee08320e3db9cf743525a8180465
Sanfori <micnie03@easv365.dk>
2024-01-17 21:49:17 +0100

    Style/color

--- src/sample/GUI/Controller/MovieWindowController.java
+++ src/sample/GUI/Controller/MovieWindowController.java
@@ -8,16 +8,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
-
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import java.io.File;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
